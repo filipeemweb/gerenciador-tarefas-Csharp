33b3364905a8804a3cbdc5ad6efc3a44d0ab3cdd
Filipe Lacerda <filipeem.web@gmail.com>
2021-05-24 22:32:51 -0300

    cria a primeira parte da API C# .NET Core

---
+++ GerenciadorTarefas/GerenciadorTarefas/DTO/ErroRespostaDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorTarefas.DTO
+{
+    public class ErroRespostaDto
+    {
+        public int Status { get; set; }
+        public string Erro { get; set; }
+    }
+}
